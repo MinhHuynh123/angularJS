5a9c39f2639b3491917d5195fb9041d085ce6573
minhhuynh <minhhuynh271@gmail.com>
2020-05-27 19:20:52 +1000

    files angular and app

---
+++ app.js
@@ -0,0 +1,10 @@
+(function(){
+    'use strict';
+
+    angular.model('myFirstApp',[])
+    .controller('myFirstController',function($scope){
+
+        $scope.name = "this data from controller";
+    })
+
+})();
