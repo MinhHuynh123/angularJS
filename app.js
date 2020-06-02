c82b73ca5ff8e08008ff5ee0ec8b82392fe9cc2b
minhhuynh <minhhuynh271@gmail.com>
2020-06-02 17:19:53 +1000

    test multiple value

--- app.js
+++ app.js
@@ -12,6 +12,43 @@
     myApp.controller('myCtrl',function($scope){
 
         $scope.name= "minh";
+        $scope.hello = function(){
+            return "good day";
+        };
+
+        $scope.totalNum = returnNum($scope.name);
+        $scope.calNum = function (){
+            
+           $scope.totalNum = returnNum($scope.name);
+        };
+
+        function returnNum(str){
+            var totalNum = 0;
+            for(var i=0; i< str.length;i++){
+                totalNum += str.charCodeAt(i);
+            }
+            return totalNum;
+        }
     });
 
+    myApp.controller('secApp', function($scope){
+
+        $scope.name = "From controller 2: manage subjects";
+
+        $scope.arr = {
+            fname : "minh",
+            lname : "huynh",
+            subjects:
+            [
+                { name: 'Maths', marks :90 },
+                { name: 'Bio', marks :78 },
+                { name: 'Science', marks :95 },
+                { name: 'Physics', marks :88 }
+            ],
+            fc : function(){
+                return "data could retrieve from functions";
+            }
+        }
+
+    });
 })();
